adca92d4c3b2bb3fee0d962f087c13c9f0430dd3
SUDHAGAR98 <sudhagar20398@gmail.com>
2023-04-04 20:08:18 +0530

    master code

---
+++ src/test/java/org/in/Company.java
@@ -0,0 +1,25 @@
+package org.in;
+
+public class Company {
+	private void method1() {
+   System.out.println("1");
+	}
+   private void method2() {
+   System.out.println("2");
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
